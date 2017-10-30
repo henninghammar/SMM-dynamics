b14f57059e3c4d604024407fcdbb8abfd10a16f6
Henning Hammar <henning.hammar@gmail.com>
2017-10-30 14:35:32 +0100

    Did small fix of previous commit.

--- src/savingfields.m
+++ src/savingfields.m
@@ -13,26 +13,26 @@ Smx(j)=-mz.*Sy(j)+my.*Sz(j);
 Smy(j)=-mx.*Sz(j)+mz.*Sx(j);
 Smz(j)=-my.*Sx(j)+mx.*Sy(j);
 
-dSxbarejH(:,j)=jH.*(Sy(j).*SzT-Sz(j).*SyT);
-dSybarejH(:,j)=jH.*(Sz(j).*SxT-Sx(j).*SzT);
-dSzbarejH(:,j)=jH.*(Sx(j).*SyT-Sy(j).*SxT);
-SjHx(j)=trapz(tau,dSxbarejH(:,j));
-SjHy(j)=trapz(tau,dSybarejH(:,j));
-SjHz(j)=trapz(tau,dSzbarejH(:,j));
+dSxbarejH=jH.*(Sy(j).*SzT-Sz(j).*SyT);
+dSybarejH=jH.*(Sz(j).*SxT-Sx(j).*SzT);
+dSzbarejH=jH.*(Sx(j).*SyT-Sy(j).*SxT);
+SjHx(j)=trapz(tau,dSxbarejH);
+SjHy(j)=trapz(tau,dSybarejH);
+SjHz(j)=trapz(tau,dSzbarejH);
 
-dSxbarejDMx(:,j)=-Sy(j).*(jDMx.*SyT-jDMy.*SxT)+Sz(j).*(jDMz.*SxT-jDMx.*SzT);
-dSybarejDMy(:,j)=-Sz(j).*(jDMy.*SzT-jDMz.*SyT)+Sx(j).*(jDMx.*SyT-jDMy.*SxT);
-dSzbarejDMz(:,j)=-Sx(j).*(jDMz.*SxT-jDMx.*SzT)+Sy(j).*(jDMy.*SzT-jDMz.*SyT);
-SjDMx(j)=trapz(tau,dSxbarejDMx(:,j));
-SjDMy(j)=trapz(tau,dSybarejDMy(:,j));
-SjDMz(j)=trapz(tau,dSzbarejDMz(:,j));
+dSxbarejDMx=-Sy(j).*(jDMx.*SyT-jDMy.*SxT)+Sz(j).*(jDMz.*SxT-jDMx.*SzT);
+dSybarejDMy=-Sz(j).*(jDMy.*SzT-jDMz.*SyT)+Sx(j).*(jDMx.*SyT-jDMy.*SxT);
+dSzbarejDMz=-Sx(j).*(jDMz.*SxT-jDMx.*SzT)+Sy(j).*(jDMy.*SzT-jDMz.*SyT);
+SjDMx(j)=trapz(tau,dSxbarejDMx);
+SjDMy(j)=trapz(tau,dSybarejDMy);
+SjDMz(j)=trapz(tau,dSzbarejDMz);
 
-dSxbarejIx(:,j)=Sy(j).*(jIzx.*SxT+jIzy.*SyT+jIzz.*SzT)-Sz(j).*(jIyx.*SxT+jIyy.*SyT+jIyz.*SzT);
-dSybarejIy(:,j)=Sz(j).*(jIxx.*SxT+jIxy.*SyT+jIxz.*SzT)-Sx(j).*(jIzx.*SxT+jIzy.*SyT+jIzz.*SzT);
-dSzbarejIz(:,j)=Sx(j).*(jIyx.*SxT+jIyy.*SyT+jIyz.*SzT)-Sy(j).*(jIxx.*SxT+jIxy.*SyT+jIxz.*SzT);
-SjIx(j)=trapz(tau,dSxbarejIx(:,j));
-SjIy(j)=trapz(tau,dSybarejIy(:,j));
-SjIz(j)=trapz(tau,dSzbarejIz(:,j));
+dSxbarejIx=Sy(j).*(jIzx.*SxT+jIzy.*SyT+jIzz.*SzT)-Sz(j).*(jIyx.*SxT+jIyy.*SyT+jIyz.*SzT);
+dSybarejIy=Sz(j).*(jIxx.*SxT+jIxy.*SyT+jIxz.*SzT)-Sx(j).*(jIzx.*SxT+jIzy.*SyT+jIzz.*SzT);
+dSzbarejIz=Sx(j).*(jIyx.*SxT+jIyy.*SyT+jIyz.*SzT)-Sy(j).*(jIxx.*SxT+jIxy.*SyT+jIxz.*SzT);
+SjIx(j)=trapz(tau,dSxbarejIx);
+SjIy(j)=trapz(tau,dSybarejIy);
+SjIz(j)=trapz(tau,dSzbarejIz);
 
 %The fields acting on the spin
 ejxvect(j)=ejx;
@@ -46,34 +46,6 @@ mzvect(j)=mz;
 Beffxvect(j)=Beffx;
 Beffyvect(j)=Beffy;
 Beffzvect(j)=Beffz;
-
-barejHfield(:,j)=jH;
-jHfield(j)=trapz(tau,barejHfield(:,j));
-
-barejIfieldx(:,j)=jIxx.*SxT+jIxy.*SyT+jIxz.*SzT;
-barejIfieldy(:,j)=jIyx.*SxT+jIyy.*SyT+jIyz.*SzT;
-barejIfieldz(:,j)=jIzx.*SxT+jIzy.*SyT+jIzz.*SzT;
-jIfieldx(j)=trapz(tau,barejIfieldx(:,j));
-jIfieldy(j)=trapz(tau,barejIfieldy(:,j));
-jIfieldz(j)=trapz(tau,barejIfieldz(:,j));
-
-barejDMfieldx(:,j)=jDMz.*SyT-jDMy.*SzT;
-barejDMfieldy(:,j)=jDMx.*SzT-jDMz.*SxT;
-barejDMfieldz(:,j)=jDMy.*SxT-jDMx.*SyT;
-jDMfieldx(j)=trapz(tau,barejDMfieldx(:,j));
-jDMfieldy(j)=trapz(tau,barejDMfieldy(:,j));
-jDMfieldz(j)=trapz(tau,barejDMfieldz(:,j));
-
-jHtot(:,j)=jH;
-jDMxtot(:,j)=jDMx;
-jDMytot(:,j)=jDMy;
-jDMztot(:,j)=jDMz;
-jIxxtot(:,j)=jIxx;
-jIyytot(:,j)=jIyy;
-jIzztot(:,j)=jIzz;
-jIxytot(:,j)=jIxy;
-jIxztot(:,j)=jIxz;
-jIyztot(:,j)=jIyz;
 
 jHt(j)=trapz(tau,jH);
 jDMxt(j)=trapz(tau,jDMx);
